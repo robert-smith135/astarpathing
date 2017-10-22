51e27016463f116362f3f5c85a0163109fac0af1
Robert Smith <ert135@gmail.com>
2017-10-22 12:49:13 +0100

    added file structure

---
+++ src/draw.ts
@@ -0,0 +1,33 @@
+//import typescirpt types. 
+///<reference path='../p5-global-mode.d.ts'/>
+
+//import forces
+import airResistance from './grid';
+
+//extend existing window property, we have to put the draw and setup functinos of the global window object for p5 to work in global mode
+declare global {
+    interface Window { 
+        setup: any;
+        draw: any;
+        mousePressed: any;
+        mouseReleased: any;
+        preload: any;
+        mouseClicked: any;
+    }
+}
+
+let setup = function() {
+    createCanvas(800, 800);
+}
+
+let draw = function() {
+
+}
+
+let mouseClicked = function() {
+
+}
+
+window.setup = setup;
+window.draw = draw;
+window.mouseClicked = mouseClicked;
---
+++ src/grid.ts
@@ -0,0 +1,11 @@
+export default class AirResistance  {
+    
+      constructor() {
+
+      }
+  
+      public testMethod(): void {
+
+      }
+    
+  }
